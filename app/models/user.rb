c5c9b7264273eed6743b7cbb5cb18d4bf42374ee
Jay Bednar <ironviralmedia@gmail.com>
2018-04-07 01:00:20 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,27 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+
+
+
+  def self.make_deposit(user, amount)
+    user.balance += amount
+    user.save
+  end
+
+  def self.make_withdrawal(user, amount)
+    if user.balance >= amount
+      user.balance -= amount
+      user.save
+    else
+      false
+    end
+  end
+
+  def self.show_balance
+    user = self.find(session[:user_id])
+    user.balance
+  end
+
+
 end
